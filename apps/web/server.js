981f5f2948c9c80cd021fd16dcc4e2a3214c9976
Maciej Lyskawinski <lyskawinski.maciej@gmail.com>
2026-05-13 17:43:54 +0200

    chore: canary and tagged releases

--- apps/web/server.js
+++ apps/web/server.js
@@ -1,45 +1,36 @@
-import express from 'express';
+import { createServer } from 'node:http';
 import next from 'next';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import httpProxy from 'http-proxy';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const API_URL = process.env.API_URL;
-const WS_URL = process.env.WS_URL;
+const proxy = httpProxy.createProxyServer({
+  target: process.env.WS_URL,
+  ws: true,
+});
 
 app.prepare().then(() => {
-  const server = express();
+  const port = process.env.PORT;
 
-  const apiProxy = createProxyMiddleware({
-    target: API_URL+'/api',
-    pathRewrite: function (path, req) { return path.replace('/api/proxy', '/api') },
-  });
-
-  const socketProxy = createProxyMiddleware({
-    target: WS_URL,
-    changeOrigin: true,
-    ws: true,
-    pathRewrite: { '^/socket.io': '/api/socket.io' },
-  });
-
-  server.use('/api/proxy', apiProxy);
-  server.use('/socket.io', socketProxy);
-
-  server.all('*path', (req, res) => {
-    return handle(req, res);
-  });
-
-  const PORT = process.env.PORT || 3000;
-  const httpServer = server.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
-  });
-
-  httpServer.on('upgrade', (req, socket, head) => {
-    if (req.url.startsWith('/socket.io')) {
-      socketProxy.upgrade(req, socket, head);
+  const server = createServer((req, res) => {
+    if (req.url.startsWith('/socket.io/')) {
+      proxy.web(req, res, { target: process.env.WS_URL });
+    } else if (req.url.startsWith('/api/proxy/')) {
+      req.url = req.url.replace('/api/proxy/', '/api/');
+      proxy.web(req, res, { target: process.env.API_URL });
+    } else {
+      handle(req, res);
     }
+  });
+  server.on('upgrade', (req, socket, head) => {
+    if (req.url.startsWith('/socket.io/')) {
+      proxy.ws(req, socket, head);
+    }
+  });
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
